0780bed2177d15f45568c38d462701fe120e7ae5
gunho kim <>
2021-08-15 17:27:38 +0900

    prettier , lint add

--- javaScript/type.js
+++ javaScript/type.js
@@ -1,19 +1,19 @@
-// js 동적 타입 = 반응에 쉬운 언어 
-var a = ? 
+// js 동적 타입 = 반응에 쉬운 언어
+var a = ''
 typeof a // string
+
 //
 // 원시타입 = number, string, undfinded, null, boolean, Sybol
 
 // 객체타입 = arr, obj
 
-
 // typescrip가 왜 필요한가?
-function add(a, b){
-    if(typeof a !== 'number' || typeof b !== 'number'){
-        throw new Error('인자와 값은 모두 숫자...')
-    }
-    return a + b
+function add(a, b) {
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    throw new Error('인자와 값은 모두 숫자...')
+  }
+  return a + b
 }
 
-add(10, 20) good
-add("짱","구") no
+add(10, 20) // good
+add('짱', '구') // no
