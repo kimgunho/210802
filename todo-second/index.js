546a218ea741d9c7f4b68147d4fd0b76b171ce5d
gunho kim <>
2021-08-22 04:52:30 +0900

    포켓몬 리스트구성

--- todo-second/index.js
+++ todo-second/index.js
@@ -3,6 +3,7 @@
 
 # api{pokemon.api}로 불러와보자
 # 10가지 포켓몬정보를 읽어오기
+# 속성으로 원하는 색상넣기
 # 속성으로 원하는 포켓몬 찾기
 
 render : 함수는 화면에 데이터를 그리는 용도
@@ -19,26 +20,74 @@ class Pokemons {
   }
 
   initialize() {
-    this.render()
-    this.componentBindEvent()
+    this.repeatLoadFuction()
   }
 
+  repeatLoadFuction() {
+    const repeatMaxNum = 10
+    for (let i = 1; i <= repeatMaxNum; i++) {
+      this.getPokemon(i)
+    }
+  }
+
+  getPokemon(num) {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${num}`)
+      .then((res) => res.json())
+      //   .then((data) => data)
+      .then((data) => {
+        const {
+          name,
+          id,
+          sprites: { front_default: img },
+          //   types: {
+          //     0: {
+          //       type: { name: typeName },
+          //     },
+          //   },
+        } = data
+        for (const typelist of data.types) {
+          //   console.log(typelist.type.name)
+          this.pushPokemonsList(name, id, img, typelist.type.name)
+        }
+      })
+      .catch((err) => console.log(err))
+  }
+
+  pushPokemonsList(name, id, img, type) {
+    this.pokomons = [
+      ...this.pokomons,
+      {
+        id: id,
+        name: name,
+        img: img,
+        type: type,
+      },
+    ]
+    // console.log(this.pokomons)
+    this.render()
+  }
+
+  getPokemonsType() {}
+
   render() {
-    return (document.getElementById('list').innerHTML = this.pokomons.reduce(
+    document.getElementById('list').innerHTML = this.pokomons.reduce(
       (acc, cuu) => {
         return `
             ${acc}
              <li>
                 <div>
-                    <p class='remove' data-id='${cuu.id}'>삭제</p>
+                    <p class='remove' data-id='${cuu.id}'>del</p>
+                    <img src="${cuu.img}" alt='${cuu.name}'>
                     <h2 class='name'>${cuu.name}</h2>
-                    <p class='status'>${cuu.status}</p>
-                    <p class='tx'>${cuu.tx}</p>
+                    <p class='type'>
+                        <span>${cuu.type}</span>
+                    </p>
                 </div>
             </li>                      
           `
       },
-    ))
+      ``,
+    )
   }
 }
 
